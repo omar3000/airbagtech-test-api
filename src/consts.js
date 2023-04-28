79adf117994c01bd81031745161ee5a2a30f8cfe
Omar <omar.franco@dd3tech.mx>
2023-04-27 20:39:52 -0600

    feat(repeats): remove repeats elements of array and unit test

--- src/consts.js
+++ src/consts.js
@@ -1,7 +1,8 @@
 const type = {
     COMBINATIONS: 1,
     BLACK_JACK: 2,
-    CESAR: 3
+    CESAR: 3,
+    REPEAT: 4
 }
 
 module.exports = { type }
---
+++ src/controllers/repeat.controller.js
@@ -0,0 +1,29 @@
+const { searchHistory, saveHistory } = require('../dbutils.js')
+const { v4: uuidv4 } = require('uuid');
+const { type } = require('../consts.js');
+
+
+async function removeRepeats(req, res) {
+
+    try{
+    
+        resultHistory = await searchHistory(req.body.array.join(','), type.REPEAT,null);
+    
+        if(resultHistory){
+            res.status(200).json(resultHistory.output);
+            return;
+        }
+        const arraySinRepetidos = [...new Set(req.body.array)]
+    
+        await saveHistory({id: uuidv4(), input: req.body.array.join(','), output: arraySinRepetidos.join(','), userid: req.userData.userId, type: type.REPEAT})
+          
+        res.status(200).json(arraySinRepetidos.join(','));
+    }
+    catch(error){
+        console.log(error)
+        res.status(500).send(error)
+    }
+
+}
+
+module.exports = { removeRepeats }
