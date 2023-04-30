84b2d97a784b1bea49a6e6896ad457adb9c7ba0c
Omar <omar.franco@dd3tech.mx>
2023-04-30 12:26:56 -0600

    refactor(blackjack): add try-catch validation

--- src/controllers/blackjack.controller.js
+++ src/controllers/blackjack.controller.js
@@ -12,42 +12,47 @@ async function valueHandBlackjack(req, res) {
     return;
   }
 
-  let cards = req.body.cards;
-  console.log(cards);
+  try{
+    let cards = req.body.cards;
+    console.log(cards);
 
-  const resultHistory = await searchHistory(cards.join(','), type.BLACK_JACK,null);
+    const resultHistory = await searchHistory(cards.join(','), type.BLACK_JACK,null);
 
-  if(resultHistory){
-    res.status(200).json(resultHistory.output);
-    return;
-  }
-
-  // Inicializamos el valor de la mano a cero y contamos el número de Ases
-  let value = 0;
-  let numAses = 0;
-    
-  // Recorremos las cartas y calculamos su valor
-  for (let card of cards) {
-    if (card === cardType.AS) {
-      numAses++;
-      value += 11;
-    } else if (card === cardType.JACK || card === cardType.QUEEN || card === cardType.KING) {
-      value += 10;
-    } else {
-      value += parseInt(card);
+    if(resultHistory){
+      res.status(200).json(resultHistory.output);
+      return;
     }
-  }
+
+    // Inicializamos el valor de la mano a cero y contamos el número de Ases
+    let value = 0;
+    let numAses = 0;
     
-  // Si tenemos algún As y el valor de la mano supera 21, restamos 10 al valor de cada As
-  while (numAses > 0 && value > 21) {
-    value -= 10;
-    numAses--;
-  }
+    // Recorremos las cartas y calculamos su valor
+    for (let card of cards) {
+      if (card === cardType.AS) {
+        numAses++;
+        value += 11;
+      } else if (card === cardType.JACK || card === cardType.QUEEN || card === cardType.KING) {
+        value += 10;
+      } else {
+        value += parseInt(card);
+      }
+    }
+    
+    // Si tenemos algún As y el valor de la mano supera 21, restamos 10 al valor de cada As
+    while (numAses > 0 && value > 21) {
+      value -= 10;
+      numAses--;
+    }
 
-  await saveHistory({id: uuidv4(), input: cards.join(','),  output: value.toString(), userid: req.userData.userId, type: type.BLACK_JACK});
+    await saveHistory({id: uuidv4(), input: cards.join(','),  output: value.toString(), userid: req.userData.userId, type: type.BLACK_JACK});
       
-  res.status(200).json(value.toString());
+    res.status(200).json(value.toString());
+  }
+  catch(error){
+    console.log(error);
+    res.status(500).send(error);
+  }
 }
-
 
 module.exports = { valueHandBlackjack };
